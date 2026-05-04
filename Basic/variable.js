93cbaaf518bdf03287207f78141e521e72a0a9d8
prathamsuthar <prathamsuthar384@gmail.com>
2026-05-04 12:22:07 +0530

    learn variable in js

---
+++ Basic/variable.js
@@ -0,0 +1,36 @@
+console.log("Hello from console.log");
+
+var value = 10;
+var value = 20;
+
+console.log(value);
+
+{
+  let a = 10;
+  console.log(a);
+}
+// console.log(a);
+
+let a = 12;
+a = 13;
+
+console.log(b); // undefined
+var b = 5;
+// // With semicolon
+// alert('Hello');
+// alert('World');
+
+// // without semicolon
+// alert('Hello')
+// alert('World')
+
+let var1 = 100;
+let var2 = var1;
+console.log(var2);
+
+let obj = { x: 1, y: 2 };
+let obj2 = obj;
+console.log(obj2.x, obj.y);
+
+console.log(f);
+var f = 5;
