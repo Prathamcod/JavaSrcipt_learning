f8f2f1411b1dfc451481b5a5cc0b806eb9a81957
prathamsuthar <prathamsuthar384@gmail.com>
2026-05-07 11:03:32 +0530

    practice of primitive data type

--- Basic/primitivedatatype.js
+++ Basic/primitivedatatype.js
@@ -70,32 +70,11 @@ console.log(Math.min(10, 20, 5));
 
 console.log(Math.pow(2, 4));
 
-//  Math.sqrt
-console.log(Math.sqrt(16));
-
-let otp = Math.floor(Math.random() * 10000);
-console.log(otp);
-console.log(Math.floor(5.5));
-console.log(Math.ceil(5.5));
-
-console.log("Ceil ", Math.ceil(4.634545454));
-console.log("Ceil 2", Math.ceil(3.3434545301111));
-
-const sixDigitOtp = Math.floor(Math.random() * 1000000);
-console.log(sixDigitOtp);
-
-const hexa = 0xff;
-console.log(hexa);
-
-const num = 10;
-console.log(num.toString(2));
-
-console.log(num.toString(8));
-
-let x = 255;
-console.log(x.toString(16));
-
-console.log((255).toString(16));
-
 const color = " #FF0000";
 console.log(color.toString(2));
+
+const binary = 0b110011110;
+
+console.log(binary.toString(16));
+console.log(binary.toString(8));
+console.log(binary.toString(2));
